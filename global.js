6cb97fdab8cec28f7c66c24440670a02b11bfea9
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-15 21:47:22 -0700

    Renamed globe.js to global. Added __dirname and __filename properties to it

---
+++ global.js
@@ -0,0 +1,20 @@
+// Global object
+// In node we get access to the global object via different methods and properties
+// In a way, it's like but different then the window object inside browser - browser's global object
+// No DOM methods like browser global object - Ex. document is only in the window object
+
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log("in the timeout");
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log("in the interval");
+// }, 1000);
+
+// __dirname and __filename
+
+console.log(__dirname);
+console.log(__filename);
