e5cae9eb6e96a313e63cdb0c2f8d2a05b71d817a
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-15 22:02:34 -0700

    Finished modules and require section - video #2

---
+++ modules.js
@@ -0,0 +1,8 @@
+const { people, ages } = require("./people.js"); // older import style
+
+console.log(people, ages);
+
+const os = require("os");
+console.log(os);
+console.log(os.platform());
+console.log(os.homedir());
