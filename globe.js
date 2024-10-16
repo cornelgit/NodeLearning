2f56a037cd3e6ef0b414c6bbb4c1befe0a2a98ca
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-15 21:35:22 -0700

    removed test.js and added globa.js for global object file

---
+++ globe.js
@@ -0,0 +1,3 @@
+// Global object
+// In node we get access to the global object via different methods and properties
+// In a way, it's like the window object inside browser - browser's global object
