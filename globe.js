7f65a777665a54d668317f9f67eeea027c944775
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-15 21:40:10 -0700

    Commented out global console log print

--- globe.js
+++ globe.js
@@ -1,3 +1,9 @@
 // Global object
 // In node we get access to the global object via different methods and properties
 // In a way, it's like the window object inside browser - browser's global object
+
+// console.log(global);
+
+setTimeout(() => {
+    console.log("in the timeout");
+}, 3000);
