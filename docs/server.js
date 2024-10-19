e921b71d304a1ec1f1de5bd10c1cb80ffc1b10e8
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-18 22:55:30 -0700

    Added server listen logic

--- docs/server.js
+++ docs/server.js
@@ -1,3 +1,12 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((request, response) => {
+    console.log("Request made!");
+});
+
+// Need to listen on a port #
+// localhost is the default argument
+// the callback function fires when we start listening
+server.listen(3000, "localhost", () => {
+    console.log("Listening for requests on port 3000...");
+});
