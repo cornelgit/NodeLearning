8c293444519c9b38f73f5fe83adaf7d9f3d6dfbb
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-20 09:15:14 -0700

    Added more html tags to response.write for examples

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = http.createServer((request, response) => {
 
     // set header content type
     response.setHeader("Content-Type", "text/html"); // first set header  type can vary: 'text/plain' or 'text/html' for example
+    response.write("<head><link rel='stylesheet' href='#'><head>"); // write the response to be sent back to browser
     response.write("<p>Hi there. This is a response.<p>"); // write the response to be sent back to browser
     response.write("<p>Hello again.<p>"); // write the response to be sent back to browser
     response.end(); // end the response and send back
