f42be3a4a2528c85e58c60946f7dc64a63b62efa
Cornel Stoica <cornel.stoica@oit.edu>
2024-10-21 23:09:55 -0700

    Updated server.js to send back an html to client

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((request, response) => {
     console.log(request.url, request.method);
 
+    // // set header content type
+    // response.setHeader("Content-Type", "text/html"); // first set header  type can vary: 'text/plain' or 'text/html' for example
+    // response.write("<head><link rel='stylesheet' href='#'><head>"); // write the response to be sent back to browser
+    // response.write("<p>Hi there. This is a response.<p>"); // write the response to be sent back to browser
+    // response.write("<p>Hello again.<p>"); // write the response to be sent back to browser
+    // response.end(); // end the response and send back
+
     // set header content type
-    response.setHeader("Content-Type", "text/html"); // first set header  type can vary: 'text/plain' or 'text/html' for example
-    response.write("<head><link rel='stylesheet' href='#'><head>"); // write the response to be sent back to browser
-    response.write("<p>Hi there. This is a response.<p>"); // write the response to be sent back to browser
-    response.write("<p>Hello again.<p>"); // write the response to be sent back to browser
-    response.end(); // end the response and send back
+    response.setHeader("Content-Type", "text/html");
+
+    // send an HTML file
+    fs.readFile("./views/index.html", (error, data) => {
+        if (error) {
+            console.log(error);
+            response.end();
+        } else {
+            //response.write(data); // we technically don't need this if used only once
+            response.end(data); // pass it here instead, same functionality for 1 writable item
+        }
+    });
 });
 
 // Need to listen on a port #
