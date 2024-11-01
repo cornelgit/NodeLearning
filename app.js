60ee25add61e0098c33be117cf7880d038c1f77c
Cornel Stoica <cornel.stoica@oit.edu>
2024-11-01 13:03:05 -0700

    Registered view engine

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
+
+app.use((req, res) => {
+    console.log("New Request Made:");
+    console.log("Host:", req.hostname);
+    console.log("Path:", req.path);
+    console.log("Method:", req.method);
+});
 
 app.get("/", (req, res) => {
     //res.send("<p>home page</p>");
@@ -21,7 +31,7 @@ app.get("/about-us", (req, res) => {
     res.redirect("/about");
 });
 
-// this fires for every request up to here - must be at the bottom
+// this fires for every type of request (including post) top to bottom until we send a reponse or close program/browser- should be at the bottom
 // 404 page - use use() method - to fire middleware - top down this is the last match in thie file
 app.use((req, res) => {
     res.statusCode(404).sendFile("./views/404.html", { root: __dirname }); //we have to manually set a 4404 error, the html filename isn't a way to tell express
